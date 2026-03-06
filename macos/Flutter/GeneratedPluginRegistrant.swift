d204d2175956d4bf65599ddb8a3be9f321b731c0
M. Ramadhan Aditya P <114163931+muhammadramadhanap14@users.noreply.github.com>
2026-03-07 00:20:11 +0700

    commit -- Remove unused dependencies including flutter_secure_storage, convex_bottom_bar, flutter_staggered_animations, and flutter_keyboard_visibility, and update path_provider to transitive usage

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,12 @@ import Foundation
 
 import connectivity_plus
 import device_info_plus
-import flutter_secure_storage_darwin
 import package_info_plus
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
