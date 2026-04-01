1c81b11c7715173c207a9d13e22448fc7802392c
M. Ramadhan Aditya P <114163931+muhammadramadhanap14@users.noreply.github.com>
2026-04-01 15:32:53 +0700

    commit

---
+++ lib/l10n/app_localizations.dart
@@ -0,0 +1,632 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart' as intl;
+
+import 'app_localizations_en.dart';
+import 'app_localizations_id.dart';
+
+// ignore_for_file: type=lint
+
+/// Callers can lookup localized strings with an instance of AppLocalizations
+/// returned by `AppLocalizations.of(context)`.
+///
+/// Applications need to include `AppLocalizations.delegate()` in their app's
+/// `localizationDelegates` list, and the locales they support in the app's
+/// `supportedLocales` list. For example:
+///
+/// ```dart
+/// import 'l10n/app_localizations.dart';
+///
+/// return MaterialApp(
+///   localizationsDelegates: AppLocalizations.localizationsDelegates,
+///   supportedLocales: AppLocalizations.supportedLocales,
+///   home: MyApplicationHome(),
+/// );
+/// ```
+///
+/// ## Update pubspec.yaml
+///
+/// Please make sure to update your pubspec.yaml to include the following
+/// packages:
+///
+/// ```yaml
+/// dependencies:
+///   # Internationalization support.
+///   flutter_localizations:
+///     sdk: flutter
+///   intl: any # Use the pinned version from flutter_localizations
+///
+///   # Rest of dependencies
+/// ```
+///
+/// ## iOS Applications
+///
+/// iOS applications define key application metadata, including supported
+/// locales, in an Info.plist file that is built into the application bundle.
+/// To configure the locales supported by your app, you’ll need to edit this
+/// file.
+///
+/// First, open your project’s ios/Runner.xcworkspace Xcode workspace file.
+/// Then, in the Project Navigator, open the Info.plist file under the Runner
+/// project’s Runner folder.
+///
+/// Next, select the Information Property List item, select Add Item from the
+/// Editor menu, then select Localizations from the pop-up menu.
+///
+/// Select and expand the newly-created Localizations item then, for each
+/// locale your application supports, add a new item and select the locale
+/// you wish to add from the pop-up menu in the Value field. This list should
+/// be consistent with the languages listed in the AppLocalizations.supportedLocales
+/// property.
+abstract class AppLocalizations {
+  AppLocalizations(String locale)
+    : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+
+  final String localeName;
+
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  /// A list of this localizations delegate along with the default localizations
+  /// delegates.
+  ///
+  /// Returns a list of localizations delegates containing this delegate along with
+  /// GlobalMaterialLocalizations.delegate, GlobalCupertinoLocalizations.delegate,
+  /// and GlobalWidgetsLocalizations.delegate.
+  ///
+  /// Additional delegates can be added by appending to this list in
+  /// MaterialApp. This list does not have to be used at all if a custom list
+  /// of delegates is preferred or required.
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
+        delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ];
+
+  /// A list of this localizations delegate's supported locales.
+  static const List<Locale> supportedLocales = <Locale>[
+    Locale('en'),
+    Locale('id'),
+  ];
+
+  /// No description provided for @home.
+  ///
+  /// In en, this message translates to:
+  /// **'Home'**
+  String get home;
+
+  /// No description provided for @account.
+  ///
+  /// In en, this message translates to:
+  /// **'Account'**
+  String get account;
+
+  /// No description provided for @recipeMateAi.
+  ///
+  /// In en, this message translates to:
+  /// **'RecipeMate AI'**
+  String get recipeMateAi;
+
+  /// No description provided for @menuErrorReport.
+  ///
+  /// In en, this message translates to:
+  /// **'Error Report'**
+  String get menuErrorReport;
+
+  /// No description provided for @security.
+  ///
+  /// In en, this message translates to:
+  /// **'Security'**
+  String get security;
+
+  /// No description provided for @language.
+  ///
+  /// In en, this message translates to:
+  /// **'Language'**
+  String get language;
+
+  /// No description provided for @theme.
+  ///
+  /// In en, this message translates to:
+  /// **'Theme'**
+  String get theme;
+
+  /// No description provided for @logout.
+  ///
+  /// In en, this message translates to:
+  /// **'Logout'**
+  String get logout;
+
+  /// No description provided for @selectLanguage.
+  ///
+  /// In en, this message translates to:
+  /// **'Select Language'**
+  String get selectLanguage;
+
+  /// No description provided for @selectTheme.
+  ///
+  /// In en, this message translates to:
+  /// **'Select Theme'**
+  String get selectTheme;
+
+  /// No description provided for @changeFoodTypes.
+  ///
+  /// In en, this message translates to:
+  /// **'Change food types & dietary preferences'**
+  String get changeFoodTypes;
+
+  /// No description provided for @stConfirmLogout.
+  ///
+  /// In en, this message translates to:
+  /// **'Are you sure you want to log out?'**
+  String get stConfirmLogout;
+
+  /// No description provided for @stConfirmChange.
+  ///
+  /// In en, this message translates to:
+  /// **'Are you sure you want to change?'**
+  String get stConfirmChange;
+
+  /// No description provided for @stTimeOutConnection.
+  ///
+  /// In en, this message translates to:
+  /// **'Connection timeout, please try again later'**
+  String get stTimeOutConnection;
+
+  /// No description provided for @stNoConnectionMessage.
+  ///
+  /// In en, this message translates to:
+  /// **'Failed to connect to server,\nplease check your internet connection again'**
+  String get stNoConnectionMessage;
+
+  /// No description provided for @stNotFound.
+  ///
+  /// In en, this message translates to:
+  /// **'No Data Found'**
+  String get stNotFound;
+
+  /// No description provided for @confirmLogout.
+  ///
+  /// In en, this message translates to:
+  /// **'Yes, Log out'**
+  String get confirmLogout;
+
+  /// No description provided for @yesBtn.
+  ///
+  /// In en, this message translates to:
+  /// **'Yes'**
+  String get yesBtn;
+
+  /// No description provided for @stCancelTitle.
+  ///
+  /// In en, this message translates to:
+  /// **'Cancel'**
+  String get stCancelTitle;
+
+  /// No description provided for @backBtnTitle.
+  ///
+  /// In en, this message translates to:
+  /// **'Back'**
+  String get backBtnTitle;
+
+  /// No description provided for @stNextBtn.
+  ///
+  /// In en, this message translates to:
+  /// **'Next'**
+  String get stNextBtn;
+
+  /// No description provided for @stFinishBtn.
+  ///
+  /// In en, this message translates to:
+  /// **'Finish!'**
+  String get stFinishBtn;
+
+  /// No description provided for @stRecommended.
+  ///
+  /// In en, this message translates to:
+  /// **'Recommended for you'**
+  String get stRecommended;
+
+  /// No description provided for @stTopSearching.
+  ///
+  /// In en, this message translates to:
+  /// **'Top searching food'**
+  String get stTopSearching;
+
+  /// No description provided for @stSeeAll.
+  ///
+  /// In en, this message translates to:
+  /// **'See all'**
+  String get stSeeAll;
+
+  /// No description provided for @stSearchRecipes.
+  ///
+  /// In en, this message translates to:
+  /// **'Search recipes, ingredients...'**
+  String get stSearchRecipes;
+
+  /// No description provided for @stMorningGreeting.
+  ///
+  /// In en, this message translates to:
+  /// **'GOOD MORNING'**
+  String get stMorningGreeting;
+
+  /// No description provided for @stDayLightGreeting.
+  ///
+  /// In en, this message translates to:
+  /// **'GOOD AFTERNOON'**
+  String get stDayLightGreeting;
+
+  /// No description provided for @stAfternoonGreeting.
+  ///
+  /// In en, this message translates to:
+  /// **'GOOD EVENING'**
+  String get stAfternoonGreeting;
+
+  /// No description provided for @stEveningGreeting.
+  ///
+  /// In en, this message translates to:
+  /// **'GOOD NIGHT'**
+  String get stEveningGreeting;
+
+  /// No description provided for @stWelcomeBack.
+  ///
+  /// In en, this message translates to:
+  /// **'Welcome back'**
+  String get stWelcomeBack;
+
+  /// No description provided for @stWelcomeGreet.
+  ///
+  /// In en, this message translates to:
+  /// **'Your smart kitchen assistant awaits.'**
+  String get stWelcomeGreet;
+
+  /// No description provided for @stRegister.
+  ///
+  /// In en, this message translates to:
+  /// **'Create Account'**
+  String get stRegister;
+
+  /// No description provided for @stRegisterGreet.
+  ///
+  /// In en, this message translates to:
+  /// **'Start your smart cooking journey today.'**
+  String get stRegisterGreet;
+
+  /// No description provided for @stFullName.
+  ///
+  /// In en, this message translates to:
+  /// **'FULL NAME'**
+  String get stFullName;
+
+  /// No description provided for @stShortname.
+  ///
+  /// In en, this message translates to:
+  /// **'SHORT NAME'**
+  String get stShortname;
+
+  /// No description provided for @stAge.
+  ///
+  /// In en, this message translates to:
+  /// **'AGE'**
+  String get stAge;
+
+  /// No description provided for @stHeight.
+  ///
+  /// In en, this message translates to:
+  /// **'HEIGHT (CM)'**
+  String get stHeight;
+
+  /// No description provided for @stWeight.
+  ///
+  /// In en, this message translates to:
+  /// **'WEIGHT (KG)'**
+  String get stWeight;
+
+  /// No description provided for @stEmailAddress.
+  ///
+  /// In en, this message translates to:
+  /// **'EMAIL ADDRESS'**
+  String get stEmailAddress;
+
+  /// No description provided for @stPassword.
+  ///
+  /// In en, this message translates to:
+  /// **'PASSWORD'**
+  String get stPassword;
+
+  /// No description provided for @stForgotPassword.
+  ///
+  /// In en, this message translates to:
+  /// **'Forgot Password?'**
+  String get stForgotPassword;
+
+  /// No description provided for @stSignIn.
+  ///
+  /// In en, this message translates to:
+  /// **'Sign In'**
+  String get stSignIn;
+
+  /// No description provided for @stSignUp.
+  ///
+  /// In en, this message translates to:
+  /// **'Sign Up'**
+  String get stSignUp;
+
+  /// No description provided for @stDontHaveAccount.
+  ///
+  /// In en, this message translates to:
+  /// **'Don\'t have an account? '**
+  String get stDontHaveAccount;
+
+  /// No description provided for @stAlreadyHaveAccount.
+  ///
+  /// In en, this message translates to:
+  /// **'Already have an account? '**
+  String get stAlreadyHaveAccount;
+
+  /// No description provided for @stStep1of3.
+  ///
+  /// In en, this message translates to:
+  /// **'STEP 1 OF 3'**
+  String get stStep1of3;
+
+  /// No description provided for @stStep2of3.
+  ///
+  /// In en, this message translates to:
+  /// **'STEP 2 OF 3'**
+  String get stStep2of3;
+
+  /// No description provided for @stStep3of3.
+  ///
+  /// In en, this message translates to:
+  /// **'STEP 3 OF 3'**
+  String get stStep3of3;
+
+  /// No description provided for @stgreetPrefFood.
+  ///
+  /// In en, this message translates to:
+  /// **'Tell us about yourself!'**
+  String get stgreetPrefFood;
+
+  /// No description provided for @stgreetPrefFood2.
+  ///
+  /// In en, this message translates to:
+  /// **'Help us personalize your experience by providing some basic information'**
+  String get stgreetPrefFood2;
+
+  /// No description provided for @stgreetPrefFood3.
+  ///
+  /// In en, this message translates to:
+  /// **'Tell us your taste!'**
+  String get stgreetPrefFood3;
+
+  /// No description provided for @stgreetPrefFood4.
+  ///
+  /// In en, this message translates to:
+  /// **'What are your favorite food types or dietary preferences?'**
+  String get stgreetPrefFood4;
+
+  /// No description provided for @stBiometric.
+  ///
+  /// In en, this message translates to:
+  /// **'BIOMETRIC AUTHENTICATION'**
+  String get stBiometric;
+
+  /// No description provided for @stBiometricFace.
+  ///
+  /// In en, this message translates to:
+  /// **'Enable Face ID'**
+  String get stBiometricFace;
+
+  /// No description provided for @stBiometricFingerPrint.
+  ///
+  /// In en, this message translates to:
+  /// **'Enable Fingerprint ID'**
+  String get stBiometricFingerPrint;
+
+  /// No description provided for @stPasswordManagement.
+  ///
+  /// In en, this message translates to:
+  /// **'PASSWORD MANAGEMENT'**
+  String get stPasswordManagement;
+
+  /// No description provided for @stChangePassword.
+  ///
+  /// In en, this message translates to:
+  /// **'Change Password'**
+  String get stChangePassword;
+
+  /// No description provided for @stChangePhoto.
+  ///
+  /// In en, this message translates to:
+  /// **'Change Profile Photo'**
+  String get stChangePhoto;
+
+  /// No description provided for @stRemovPhoto.
+  ///
+  /// In en, this message translates to:
+  /// **'Remove Photo'**
+  String get stRemovPhoto;
+
+  /// No description provided for @stChoosePhoto.
+  ///
+  /// In en, this message translates to:
+  /// **'Choose From Gallery'**
+  String get stChoosePhoto;
+
+  /// No description provided for @stTakePhoto.
+  ///
+  /// In en, this message translates to:
+  /// **'Take Photo'**
+  String get stTakePhoto;
+
+  /// No description provided for @stChangeData.
+  ///
+  /// In en, this message translates to:
+  /// **'Change Data'**
+  String get stChangeData;
+
+  /// No description provided for @stQuitApp.
+  ///
+  /// In en, this message translates to:
+  /// **'Exit Application'**
+  String get stQuitApp;
+
+  /// No description provided for @stDoubleTapToExit.
+  ///
+  /// In en, this message translates to:
+  /// **'Press again to exit'**
+  String get stDoubleTapToExit;
+
+  /// No description provided for @stSuccess.
+  ///
+  /// In en, this message translates to:
+  /// **'Success'**
+  String get stSuccess;
+
+  /// No description provided for @stFailed.
+  ///
+  /// In en, this message translates to:
+  /// **'Failed'**
+  String get stFailed;
+
+  /// No description provided for @stInfo.
+  ///
+  /// In en, this message translates to:
+  /// **'Info'**
+  String get stInfo;
+
+  /// No description provided for @stError.
+  ///
+  /// In en, this message translates to:
+  /// **'Error'**
+  String get stError;
+
+  /// No description provided for @stFingerprintSuccess.
+  ///
+  /// In en, this message translates to:
+  /// **'Fingerprint has been successfully activated'**
+  String get stFingerprintSuccess;
+
+  /// No description provided for @stFingerprintFailed.
+  ///
+  /// In en, this message translates to:
+  /// **'Authentication failed'**
+  String get stFingerprintFailed;
+
+  /// No description provided for @stFingerprintError.
+  ///
+  /// In en, this message translates to:
+  /// **'There is an error: '**
+  String get stFingerprintError;
+
+  /// No description provided for @stFingerprintInfo.
+  ///
+  /// In en, this message translates to:
+  /// **'Fingerprint is disabled'**
+  String get stFingerprintInfo;
+
+  /// No description provided for @stDontHaveBiometric.
+  ///
+  /// In en, this message translates to:
+  /// **'Biometric is not available on this device'**
+  String get stDontHaveBiometric;
+
+  /// No description provided for @stFailedOnBoarding.
+  ///
+  /// In en, this message translates to:
+  /// **'Failed to finish onboarding'**
+  String get stFailedOnBoarding;
+
+  /// No description provided for @stEnableFingerprint.
+  ///
+  /// In en, this message translates to:
+  /// **'Scan to activate Biometric feature'**
+  String get stEnableFingerprint;
+
+  /// No description provided for @stNoConnectionInternetTitle.
+  ///
+  /// In en, this message translates to:
+  /// **'No Internet Connection'**
+  String get stNoConnectionInternetTitle;
+
+  /// No description provided for @stNoConnectionInternetMessage.
+  ///
+  /// In en, this message translates to:
+  /// **'Please check your internet settings and try again'**
+  String get stNoConnectionInternetMessage;
+
+  /// No description provided for @stRetryBtn.
+  ///
+  /// In en, this message translates to:
+  /// **'Retry'**
+  String get stRetryBtn;
+
+  /// No description provided for @stReasonFailedPhoto.
+  ///
+  /// In en, this message translates to:
+  /// **'Failed to pick image:'**
+  String get stReasonFailedPhoto;
+
+  /// No description provided for @stNewPaswword.
+  ///
+  /// In en, this message translates to:
+  /// **'New Password'**
+  String get stNewPaswword;
+
+  /// No description provided for @stOldPassword.
+  ///
+  /// In en, this message translates to:
+  /// **'Old Password'**
+  String get stOldPassword;
+
+  /// No description provided for @confirmBtn.
+  ///
+  /// In en, this message translates to:
+  /// **'Confirm'**
+  String get confirmBtn;
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  Future<AppLocalizations> load(Locale locale) {
+    return SynchronousFuture<AppLocalizations>(lookupAppLocalizations(locale));
+  }
+
+  @override
+  bool isSupported(Locale locale) =>
+      <String>['en', 'id'].contains(locale.languageCode);
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
+
+AppLocalizations lookupAppLocalizations(Locale locale) {
+  // Lookup logic when only language code is specified.
+  switch (locale.languageCode) {
+    case 'en':
+      return AppLocalizationsEn();
+    case 'id':
+      return AppLocalizationsId();
+  }
+
+  throw FlutterError(
+    'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.',
+  );
+}
---
+++ lib/l10n/app_localizations_id.dart
@@ -0,0 +1,265 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Indonesian (`id`).
+class AppLocalizationsId extends AppLocalizations {
+  AppLocalizationsId([String locale = 'id']) : super(locale);
+
+  @override
+  String get home => 'Beranda';
+
+  @override
+  String get account => 'Akun';
+
+  @override
+  String get recipeMateAi => 'RecipeMate AI';
+
+  @override
+  String get menuErrorReport => 'Laporan Kesalahan';
+
+  @override
+  String get security => 'Keamanan';
+
+  @override
+  String get language => 'Bahasa';
+
+  @override
+  String get theme => 'Tema';
+
+  @override
+  String get logout => 'Keluar';
+
+  @override
+  String get selectLanguage => 'Pilih Bahasa';
+
+  @override
+  String get selectTheme => 'Pilih Tema';
+
+  @override
+  String get changeFoodTypes => 'Ubah jenis makanan & preferensi diet';
+
+  @override
+  String get stConfirmLogout => 'Apakah Anda yakin ingin keluar?';
+
+  @override
+  String get stConfirmChange => 'Apakah Anda yakin ingin mengubah?';
+
+  @override
+  String get stTimeOutConnection =>
+      'Waktu koneksi habis, silakan coba lagi nanti.';
+
+  @override
+  String get stNoConnectionMessage =>
+      'Gagal terhubung ke server,\nsilahkan periksa koneksi internet Anda kembali';
+
+  @override
+  String get stNotFound => 'Tidak Ada Data yang Ditemukan';
+
+  @override
+  String get confirmLogout => 'Ya, Keluar';
+
+  @override
+  String get yesBtn => 'Ya';
+
+  @override
+  String get stCancelTitle => 'Batal';
+
+  @override
+  String get backBtnTitle => 'Kembali';
+
+  @override
+  String get stNextBtn => 'Selanjutnya';
+
+  @override
+  String get stFinishBtn => 'Selesai!';
+
+  @override
+  String get stRecommended => 'Rekomendasi untuk anda';
+
+  @override
+  String get stTopSearching => 'Pencarian makanan teratas';
+
+  @override
+  String get stSeeAll => 'Lihat semua';
+
+  @override
+  String get stSearchRecipes => 'Cari resep, bahan-bahan...';
+
+  @override
+  String get stMorningGreeting => 'SELAMAT PAGI';
+
+  @override
+  String get stDayLightGreeting => 'SELAMAT SIANG';
+
+  @override
+  String get stAfternoonGreeting => 'SELAMAT SORE';
+
+  @override
+  String get stEveningGreeting => 'SELAMAT MALAM';
+
+  @override
+  String get stWelcomeBack => 'Selamat Datang Kembali';
+
+  @override
+  String get stWelcomeGreet => 'Asisten dapur pintar Anda menanti.';
+
+  @override
+  String get stRegister => 'Buat Akun';
+
+  @override
+  String get stRegisterGreet =>
+      'Mulailah perjalanan memasak cerdas Anda hari ini.';
+
+  @override
+  String get stFullName => 'NAMA LENGKAP';
+
+  @override
+  String get stShortname => 'NAMA PANGGILAN';
+
+  @override
+  String get stAge => 'UMUR';
+
+  @override
+  String get stHeight => 'TINGGI BADAN (CM)';
+
+  @override
+  String get stWeight => 'BERAT BADAN (KG)';
+
+  @override
+  String get stEmailAddress => 'ALAMAT EMAIL';
+
+  @override
+  String get stPassword => 'KATA SANDI';
+
+  @override
+  String get stForgotPassword => 'Lupa Kata Sandi?';
+
+  @override
+  String get stSignIn => 'Masuk';
+
+  @override
+  String get stSignUp => 'Daftar';
+
+  @override
+  String get stDontHaveAccount => 'Belum punya akun? ';
+
+  @override
+  String get stAlreadyHaveAccount => 'Sudah punya akun? ';
+
+  @override
+  String get stStep1of3 => 'LANGKAH 1 DARI 3';
+
+  @override
+  String get stStep2of3 => 'LANGKAH 2 DARI 3';
+
+  @override
+  String get stStep3of3 => 'LANGKAH 3 DARI 3';
+
+  @override
+  String get stgreetPrefFood => 'Ceritakan tentang dirimu!';
+
+  @override
+  String get stgreetPrefFood2 =>
+      'Bantu kami mempersonalisasi pengalaman Anda dengan memberikan beberapa informasi dasar.';
+
+  @override
+  String get stgreetPrefFood3 => 'Ceritakan selera Anda!';
+
+  @override
+  String get stgreetPrefFood4 =>
+      'Apa jenis makanan favorit atau preferensi diet Anda?';
+
+  @override
+  String get stBiometric => 'OTENTIKASI BIOMETRIK';
+
+  @override
+  String get stBiometricFace => 'Aktifkan Face ID';
+
+  @override
+  String get stBiometricFingerPrint => 'Aktifkan Fingerprint ID';
+
+  @override
+  String get stPasswordManagement => 'MANAJEMEN KATA SANDI';
+
+  @override
+  String get stChangePassword => 'Ubah Kata Sandi';
+
+  @override
+  String get stChangePhoto => 'Ubah Foto Profil';
+
+  @override
+  String get stRemovPhoto => 'Hapus Foto';
+
+  @override
+  String get stChoosePhoto => 'Pilih Dari Galeri';
+
+  @override
+  String get stTakePhoto => 'Ambil Foto';
+
+  @override
+  String get stChangeData => 'Ubah Data';
+
+  @override
+  String get stQuitApp => 'Keluar Aplikasi';
+
+  @override
+  String get stDoubleTapToExit => 'Tekan sekali lagi untuk keluar';
+
+  @override
+  String get stSuccess => 'Sukses';
+
+  @override
+  String get stFailed => 'Gagal';
+
+  @override
+  String get stInfo => 'Informasi';
+
+  @override
+  String get stError => 'Kesalahan';
+
+  @override
+  String get stFingerprintSuccess => 'Fingerprint berhasil diaktifkan';
+
+  @override
+  String get stFingerprintFailed => 'Autentikasi gagal';
+
+  @override
+  String get stFingerprintError => 'Terjadi kesalahan: ';
+
+  @override
+  String get stFingerprintInfo => 'Fingerprint dinonaktifkan';
+
+  @override
+  String get stDontHaveBiometric => 'Biometrik tidak tersedia di perangkat ini';
+
+  @override
+  String get stFailedOnBoarding => 'Gagal menyelesaikan onboarding';
+
+  @override
+  String get stEnableFingerprint => 'Pindai untuk mengaktifkan fitur Biometrik';
+
+  @override
+  String get stNoConnectionInternetTitle => 'Tidak Ada Koneksi Internet';
+
+  @override
+  String get stNoConnectionInternetMessage =>
+      'Silakan periksa pengaturan internet Anda dan coba lagi';
+
+  @override
+  String get stRetryBtn => 'Coba Lagi';
+
+  @override
+  String get stReasonFailedPhoto => 'Gagal memilih gambar:';
+
+  @override
+  String get stNewPaswword => 'Sandi Baru';
+
+  @override
+  String get stOldPassword => 'Sandi Lama';
+
+  @override
+  String get confirmBtn => 'Confirm';
+}
