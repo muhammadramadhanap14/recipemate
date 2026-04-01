be8a6a60b53d4cb4f142767780bd53994b41b7f4
axldrmwn13 <darmawanaxel13@gmail.com>
2026-04-01 14:48:18 +0700

    axel -- localization

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,265 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get account => 'Account';
+
+  @override
+  String get recipeMateAi => 'RecipeMate AI';
+
+  @override
+  String get menuErrorReport => 'Error Report';
+
+  @override
+  String get security => 'Security';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get theme => 'Theme';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get selectLanguage => 'Select Language';
+
+  @override
+  String get selectTheme => 'Select Theme';
+
+  @override
+  String get changeFoodTypes => 'Change food types & dietary preferences';
+
+  @override
+  String get stConfirmLogout => 'Are you sure you want to log out?';
+
+  @override
+  String get stConfirmChange => 'Are you sure you want to change?';
+
+  @override
+  String get stTimeOutConnection =>
+      'Connection timeout, please try again later';
+
+  @override
+  String get stNoConnectionMessage =>
+      'Failed to connect to server,\nplease check your internet connection again';
+
+  @override
+  String get stNotFound => 'No Data Found';
+
+  @override
+  String get confirmLogout => 'Yes, Log out';
+
+  @override
+  String get yesBtn => 'Yes';
+
+  @override
+  String get stCancelTitle => 'Cancel';
+
+  @override
+  String get backBtnTitle => 'Back';
+
+  @override
+  String get stNextBtn => 'Next';
+
+  @override
+  String get stFinishBtn => 'Finish!';
+
+  @override
+  String get stRecommended => 'Recommended for you';
+
+  @override
+  String get stTopSearching => 'Top searching food';
+
+  @override
+  String get stSeeAll => 'See all';
+
+  @override
+  String get stSearchRecipes => 'Search recipes, ingredients...';
+
+  @override
+  String get stMorningGreeting => 'GOOD MORNING';
+
+  @override
+  String get stDayLightGreeting => 'GOOD AFTERNOON';
+
+  @override
+  String get stAfternoonGreeting => 'GOOD EVENING';
+
+  @override
+  String get stEveningGreeting => 'GOOD NIGHT';
+
+  @override
+  String get stWelcomeBack => 'Welcome back';
+
+  @override
+  String get stWelcomeGreet => 'Your smart kitchen assistant awaits.';
+
+  @override
+  String get stRegister => 'Create Account';
+
+  @override
+  String get stRegisterGreet => 'Start your smart cooking journey today.';
+
+  @override
+  String get stFullName => 'FULL NAME';
+
+  @override
+  String get stShortname => 'SHORT NAME';
+
+  @override
+  String get stAge => 'AGE';
+
+  @override
+  String get stHeight => 'HEIGHT (CM)';
+
+  @override
+  String get stWeight => 'WEIGHT (KG)';
+
+  @override
+  String get stEmailAddress => 'EMAIL ADDRESS';
+
+  @override
+  String get stPassword => 'PASSWORD';
+
+  @override
+  String get stForgotPassword => 'Forgot Password?';
+
+  @override
+  String get stSignIn => 'Sign In';
+
+  @override
+  String get stSignUp => 'Sign Up';
+
+  @override
+  String get stDontHaveAccount => 'Don\'t have an account? ';
+
+  @override
+  String get stAlreadyHaveAccount => 'Already have an account? ';
+
+  @override
+  String get stStep1of3 => 'STEP 1 OF 3';
+
+  @override
+  String get stStep2of3 => 'STEP 2 OF 3';
+
+  @override
+  String get stStep3of3 => 'STEP 3 OF 3';
+
+  @override
+  String get stgreetPrefFood => 'Tell us about yourself!';
+
+  @override
+  String get stgreetPrefFood2 =>
+      'Help us personalize your experience by providing some basic information';
+
+  @override
+  String get stgreetPrefFood3 => 'Tell us your taste!';
+
+  @override
+  String get stgreetPrefFood4 =>
+      'What are your favorite food types or dietary preferences?';
+
+  @override
+  String get stBiometric => 'BIOMETRIC AUTHENTICATION';
+
+  @override
+  String get stBiometricFace => 'Enable Face ID';
+
+  @override
+  String get stBiometricFingerPrint => 'Enable Fingerprint ID';
+
+  @override
+  String get stPasswordManagement => 'PASSWORD MANAGEMENT';
+
+  @override
+  String get stChangePassword => 'Change Password';
+
+  @override
+  String get stChangePhoto => 'Change Profile Photo';
+
+  @override
+  String get stRemovPhoto => 'Remove Photo';
+
+  @override
+  String get stChoosePhoto => 'Choose From Gallery';
+
+  @override
+  String get stTakePhoto => 'Take Photo';
+
+  @override
+  String get stChangeData => 'Change Data';
+
+  @override
+  String get stQuitApp => 'Exit Application';
+
+  @override
+  String get stDoubleTapToExit => 'Press again to exit';
+
+  @override
+  String get stSuccess => 'Success';
+
+  @override
+  String get stFailed => 'Failed';
+
+  @override
+  String get stInfo => 'Info';
+
+  @override
+  String get stError => 'Error';
+
+  @override
+  String get stFingerprintSuccess =>
+      'Fingerprint has been successfully activated';
+
+  @override
+  String get stFingerprintFailed => 'Authentication failed';
+
+  @override
+  String get stFingerprintError => 'There is an error: ';
+
+  @override
+  String get stFingerprintInfo => 'Fingerprint is disabled';
+
+  @override
+  String get stDontHaveBiometric => 'Biometric is not available on this device';
+
+  @override
+  String get stFailedOnBoarding => 'Failed to finish onboarding';
+
+  @override
+  String get stEnableFingerprint => 'Scan to activate Biometric feature';
+
+  @override
+  String get stNoConnectionInternetTitle => 'No Internet Connection';
+
+  @override
+  String get stNoConnectionInternetMessage =>
+      'Please check your internet settings and try again';
+
+  @override
+  String get stRetryBtn => 'Retry';
+
+  @override
+  String get stReasonFailedPhoto => 'Failed to pick image:';
+
+  @override
+  String get stNewPaswword => 'New Password';
+
+  @override
+  String get stOldPassword => 'Old Password';
+
+  @override
+  String get confirmBtn => 'Confirm';
+}
