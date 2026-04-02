31cfcf38b045251dc4568119b162c96cbb8fc111
M. Ramadhan Aditya P <114163931+muhammadramadhanap14@users.noreply.github.com>
2026-04-02 19:24:41 +0700

    commit

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,167 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get account => 'Account';
+
+  @override
+  String get recipeMateAi => 'RecipeMate AI';
+
+  @override
+  String get menuErrorReport => 'Error Report';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get theme => 'Theme';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get selectLanguage => 'Select Language';
+
+  @override
+  String get changeFoodTypes => 'Change food types & dietary preferences';
+
+  @override
+  String get stConfirmLogout => 'Are you sure you want to log out?';
+
+  @override
+  String get stConfirmChange => 'Are you sure you want to change?';
+
+  @override
+  String get stTimeOutConnection =>
+      'Connection timeout, please try again later';
+
+  @override
+  String get stNoConnectionMessage =>
+      'Failed to connect to server,\nplease check your internet connection again';
+
+  @override
+  String get stNotFound => 'No Data Found';
+
+  @override
+  String get confirmLogout => 'Yes, Log out';
+
+  @override
+  String get yesBtn => 'Yes';
+
+  @override
+  String get stCancelTitle => 'Cancel';
+
+  @override
+  String get backBtnTitle => 'Back';
+
+  @override
+  String get stNextBtn => 'Next';
+
+  @override
+  String get stFinishBtn => 'Finish!';
+
+  @override
+  String get stRecommended => 'Recommended for you';
+
+  @override
+  String get stTopSearching => 'Top searching food';
+
+  @override
+  String get stSeeAll => 'See all';
+
+  @override
+  String get stSearchRecipes => 'Search recipes, ingredients...';
+
+  @override
+  String get stMorningGreeting => 'GOOD MORNING';
+
+  @override
+  String get stDayLightGreeting => 'GOOD AFTERNOON';
+
+  @override
+  String get stAfternoonGreeting => 'GOOD EVENING';
+
+  @override
+  String get stEveningGreeting => 'GOOD NIGHT';
+
+  @override
+  String get stWelcomeBack => 'Welcome back';
+
+  @override
+  String get stWelcomeGreet => 'Your smart kitchen assistant awaits.';
+
+  @override
+  String get stRegister => 'Create Account';
+
+  @override
+  String get stRegisterGreet => 'Start your smart cooking journey today.';
+
+  @override
+  String get stFullName => 'FULL NAME';
+
+  @override
+  String get stShortname => 'SHORT NAME';
+
+  @override
+  String get stAge => 'AGE';
+
+  @override
+  String get stHeight => 'HEIGHT (CM)';
+
+  @override
+  String get stWeight => 'WEIGHT (KG)';
+
+  @override
+  String get stEmailAddress => 'EMAIL ADDRESS';
+
+  @override
+  String get stPassword => 'PASSWORD';
+
+  @override
+  String get stForgotPassword => 'Forgot Password?';
+
+  @override
+  String get stSignIn => 'Sign In';
+
+  @override
+  String get stSignUp => 'Sign Up';
+
+  @override
+  String get stDontHaveAccount => 'Don\'t have an account? ';
+
+  @override
+  String get stAlreadyHaveAccount => 'Already have an account? ';
+
+  @override
+  String get stStep1of3 => 'STEP 1 OF 3';
+
+  @override
+  String get stStep2of3 => 'STEP 2 OF 3';
+
+  @override
+  String get stStep3of3 => 'STEP 3 OF 3';
+
+  @override
+  String get stgreetPrefFood => 'Tell us about yourself!';
+
+  @override
+  String get stgreetPrefFood2 =>
+      'Help us personalize your experience by providing some basic information';
+
+  @override
+  String get stgreetPrefFood3 => 'Tell us your taste!';
+
+  @override
+  String get stgreetPrefFood4 =>
+      'What are your favorite food types or dietary preferences?';
+}
---
+++ lib/l10n/app_localizations_id.dart
@@ -0,0 +1,168 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Indonesian (`id`).
+class AppLocalizationsId extends AppLocalizations {
+  AppLocalizationsId([String locale = 'id']) : super(locale);
+
+  @override
+  String get home => 'Beranda';
+
+  @override
+  String get account => 'Akun';
+
+  @override
+  String get recipeMateAi => 'RecipeMate AI';
+
+  @override
+  String get menuErrorReport => 'Laporan Kesalahan';
+
+  @override
+  String get language => 'Bahasa';
+
+  @override
+  String get theme => 'Tema';
+
+  @override
+  String get logout => 'Keluar';
+
+  @override
+  String get selectLanguage => 'Pilih Bahasa';
+
+  @override
+  String get changeFoodTypes => 'Ubah jenis makanan & preferensi diet';
+
+  @override
+  String get stConfirmLogout => 'Apakah Anda yakin ingin keluar?';
+
+  @override
+  String get stConfirmChange => 'Apakah Anda yakin ingin mengubah?';
+
+  @override
+  String get stTimeOutConnection =>
+      'Waktu koneksi habis, silakan coba lagi nanti.';
+
+  @override
+  String get stNoConnectionMessage =>
+      'Gagal terhubung ke server,\nsilahkan periksa koneksi internet Anda kembali';
+
+  @override
+  String get stNotFound => 'Tidak Ada Data yang Ditemukan';
+
+  @override
+  String get confirmLogout => 'Ya, Keluar';
+
+  @override
+  String get yesBtn => 'Ya';
+
+  @override
+  String get stCancelTitle => 'Batal';
+
+  @override
+  String get backBtnTitle => 'Kembali';
+
+  @override
+  String get stNextBtn => 'Selanjutnya';
+
+  @override
+  String get stFinishBtn => 'Selesai!';
+
+  @override
+  String get stRecommended => 'Rekomendasi untuk anda';
+
+  @override
+  String get stTopSearching => 'Pencarian makanan teratas';
+
+  @override
+  String get stSeeAll => 'Lihat semua';
+
+  @override
+  String get stSearchRecipes => 'Cari resep, bahan-bahan...';
+
+  @override
+  String get stMorningGreeting => 'SELAMAT PAGI';
+
+  @override
+  String get stDayLightGreeting => 'SELAMAT SIANG';
+
+  @override
+  String get stAfternoonGreeting => 'SELAMAT SORE';
+
+  @override
+  String get stEveningGreeting => 'SELAMAT MALAM';
+
+  @override
+  String get stWelcomeBack => 'Selamat Datang Kembali';
+
+  @override
+  String get stWelcomeGreet => 'Asisten dapur pintar Anda menanti.';
+
+  @override
+  String get stRegister => 'Buat Akun';
+
+  @override
+  String get stRegisterGreet =>
+      'Mulailah perjalanan memasak cerdas Anda hari ini.';
+
+  @override
+  String get stFullName => 'NAMA LENGKAP';
+
+  @override
+  String get stShortname => 'NAMA PANGGILAN';
+
+  @override
+  String get stAge => 'UMUR';
+
+  @override
+  String get stHeight => 'TINGGI BADAN (CM)';
+
+  @override
+  String get stWeight => 'BERAT BADAN (KG)';
+
+  @override
+  String get stEmailAddress => 'ALAMAT EMAIL';
+
+  @override
+  String get stPassword => 'KATA SANDI';
+
+  @override
+  String get stForgotPassword => 'Lupa Kata Sandi?';
+
+  @override
+  String get stSignIn => 'Masuk';
+
+  @override
+  String get stSignUp => 'Daftar';
+
+  @override
+  String get stDontHaveAccount => 'Belum punya akun? ';
+
+  @override
+  String get stAlreadyHaveAccount => 'Sudah punya akun? ';
+
+  @override
+  String get stStep1of3 => 'LANGKAH 1 DARI 3';
+
+  @override
+  String get stStep2of3 => 'LANGKAH 2 DARI 3';
+
+  @override
+  String get stStep3of3 => 'LANGKAH 3 DARI 3';
+
+  @override
+  String get stgreetPrefFood => 'Ceritakan tentang dirimu!';
+
+  @override
+  String get stgreetPrefFood2 =>
+      'Bantu kami mempersonalisasi pengalaman Anda dengan memberikan beberapa informasi dasar.';
+
+  @override
+  String get stgreetPrefFood3 => 'Ceritakan selera Anda!';
+
+  @override
+  String get stgreetPrefFood4 =>
+      'Apa jenis makanan favorit atau preferensi diet Anda?';
+}
